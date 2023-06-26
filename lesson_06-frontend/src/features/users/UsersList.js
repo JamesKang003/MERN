0266174f6d2b54fde1f93748c6fd782f257f60fa
JamesKang003 <kangjames5@gmail.com>
2023-06-26 19:55:39 +0900

    UsersList completed

--- lesson_06-frontend/src/features/users/UsersList.js
+++ lesson_06-frontend/src/features/users/UsersList.js
@@ -1,48 +1,50 @@
-import { useGetUsersQuery } from "./usersApiSlice"
-import User from './User'
+import { useGetUsersQuery } from "./usersApiSlice";
+import User from "./User";
 
 const UsersList = () => {
-
     const {
         data: users,
         isLoading,
         isSuccess,
         isError,
-        error
-    } = useGetUsersQuery()
+        error,
+    } = useGetUsersQuery();
 
-    let content
+    let content;
 
-    if (isLoading) content = <p>Loading...</p>
+    if (isLoading) content = <p>Loading...</p>;
 
     if (isError) {
-        content = <p className="errmsg">{error?.data?.message}</p>
+        content = <p className="errmsg">{error?.data?.message}</p>;
     }
 
     if (isSuccess) {
-
-        const { ids } = users
+        const { ids } = users;
 
         const tableContent = ids?.length
-            ? ids.map(userId => <User key={userId} userId={userId} />)
-            : null
+            ? ids.map((userId) => <User key={userId} userId={userId} />)
+            : null;
 
         content = (
             <table className="table table--users">
                 <thead className="table__thead">
                     <tr>
-                        <th scope="col" className="table__th user__username">Username</th>
-                        <th scope="col" className="table__th user__roles">Roles</th>
-                        <th scope="col" className="table__th user__edit">Edit</th>
+                        <th scope="col" className="table__th user__username">
+                            Username
+                        </th>
+                        <th scope="col" className="table__th user__roles">
+                            Roles
+                        </th>
+                        <th scope="col" className="table__th user__edit">
+                            Edit
+                        </th>
                     </tr>
                 </thead>
-                <tbody>
-                    {tableContent}
-                </tbody>
+                <tbody>{tableContent}</tbody>
             </table>
-        )
+        );
     }
 
-    return content
-}
-export default UsersList
+    return content;
+};
+export default UsersList;
